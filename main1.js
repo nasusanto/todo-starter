55d84a5989198caebf0ecccbe76a49faaaad2d07
Nadia Susanto <nasusanto@gmail.com>
2021-03-04 10:38:16 -0500

    added new list

--- main1.js
+++ main1.js
@@ -2,3 +2,13 @@ function addNewList() {
     alert('hello world alert!');
     console.log('hello world console');
 }
+
+function addListItem() {
+    console.log('hello world');
+    let list = document.getElementById("grocery-list");
+    let itemInput = document.getElementById("new-list-item");
+    let newItem = document.createElement("li");
+    newItem.appendChild(document.createTextNode(itemInput.value));
+    list.appendChild(newItem);
+
+}
